c3696a9b6fcbeb20781b36c74bc711a72d8a01c7
mac <mac@macs-MBP.fios-router.home>
2020-12-08 09:22:56 -0500

    expanding settings

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
+//LIBRARIES AND FUCTIONS
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, withRouter, Redirect } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
+//PUBLIC ROUTES
 import Home from './components/Home'
 import CartConsumer from './components/CartConsumerScreen'
 import NavigationBar from './components/subComponents/navigationBar'
 import AddVendorPage from './components/AddVendor'
+
+//ACCESS ROUTES
 import Login from './components/Login';
 import Signup from './components/Signup';
+
+//PRIVATE ROUTES
 import Profile from './components/Profile'
+import EditName from './components/subComponents/profile/editName';
+import EditEmail from './components/subComponents/profile/editEmail';
 
+//UTILITIES AND DECLARATIONS
 import { auth } from './services/firebase';
-
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import ourReducer from './store/reducer';
 const store = createStore(ourReducer);
 
@@ -77,12 +85,20 @@ class App extends Component {
           <Router>
           <NavigationBar />
           <Switch>
+
+              {/**ACCESSIBLE ANYWHERE ROUTES */}
               <Route exact path="/home"  component={Home} />
               <Route path="/cart"  component={CartConsumer} />
               <Route exact path="/addVendor" component={AddVendorPage} />
+              
+              {/**ACCESS PRIVATE ROUTES */}
               <PublicRoute exact path="/login" authenticated={this.state.authenticated} component={Login} />
               <PublicRoute exact path="/signup" authenticated={this.state.authenticated}  component={Signup} />
+              
+              {/**PROFILE ROUTES --WILL BE CHANGED TO DYNAMIC */}
               <PrivateRoute exact path="/profile" authenticated={this.state.authenticated}  component={Profile} />
+              <PrivateRoute exacr path="/profile/editName" authenticated={this.state.authenticated}  component={EditName} />
+              <PrivateRoute exacr path="/profile/editEmail" authenticated={this.state.authenticated}  component={EditEmail} />
             </Switch>
           </Router>
         </div>
